a6398cce43fca30161d1d401493610b085a4f139
tatar008 <jirapat_ja66@live.rmutl.ac.th>
2023-08-08 09:58:19 +0700

    hw04-4

--- Homework/HW04/hw04-4-triangle.cpp
+++ Homework/HW04/hw04-4-triangle.cpp
@@ -16,14 +16,14 @@
 #include <stdio.h>
 
 int main() {
-    float height, base, area;
+    float height, base, area ;
 
-    printf("Enter H & W :\n");
-    scanf("%f %f", &height, &base);
+    printf("Enter H & W : ");
+    scanf("%f %f", &height, &base) ;
 
     area = (height * base) / 2.0;
 
-    printf("Triangle Area : %.1f\n", area);
+    printf("Triangle Area : %.1f\n", area) ;
 
-    return 0;
+    return 0 ;
 }
